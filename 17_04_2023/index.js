58008ceb97846226c9a88c7d9aea82ccbbd070b5
dinesh0085 <105920688+dinesh0085@users.noreply.github.com>
2023-04-17 15:03:50 +0530

    Created Users json from that data created new collection with name and email address and then calculated age in years

---
+++ 17_04_2023/index.js
@@ -0,0 +1,39 @@
+// Imported user data from userData.json file
+const userData=require("./usersData.json")
+
+// Create Array of Objects, Objects looks like {name:dinesh,email:jdsolankijd123@gmail.com}
+
+let newObject=[]
+
+userData.users.map((data)=>{
+    newObject.push({name:data.name,email:data.name})
+})
+
+// Find age of all users.
+
+
+
+
+function calculateAge(data){
+    data.map((data)=>{
+        // let birthDate=parseInt((data.dob).substring(0,4),10);
+        let birthMonth=parseInt((data.dob).substring(3,5),10);
+        let birthYear=parseInt((data.dob).substring(6),10);
+        let currentDate=new Date()
+        // let currentDateDay=currentDate.getDate()
+        let currentDateMonth=currentDate.getMonth()
+        let currentDateYear=currentDate.getFullYear()
+        let years=currentDateYear-birthYear
+        // let months=currentDateMonth-birthMonth
+        // let days=currentDateDay-birthDate
+        
+        if(birthMonth>currentDateMonth){
+         years=years-1
+        }
+        console.log(`${data.name} Age is ${years} years`);
+    })
+}
+
+calculateAge(userData.users)
+
+// console.log(newObject);
